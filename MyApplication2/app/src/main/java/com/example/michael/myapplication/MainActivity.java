65674b0691e44f4428591bd90070339c2d5e8425
KatGunz <michaelxbauer@gmail.com>
2016-12-19 19:31:09 -0500

    barely changed a thingy

--- MyApplication2/app/src/main/java/com/example/michael/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/michael/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "poop";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +16,8 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
+       // EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = "Faggot";
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
